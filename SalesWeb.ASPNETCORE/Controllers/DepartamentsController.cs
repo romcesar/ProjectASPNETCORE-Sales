d00adeede8625be6aba7b8d5ca20d1f08af0397b
romcesar <romulo.coa@hotmail.com>
2022-12-04 18:12:23 -0300

    fisrt project

---
+++ SalesWeb.ASPNETCORE/Controllers/DepartamentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SalesWeb.ASPNETCORE.Models.Entities;
+
+namespace SalesWeb.ASPNETCORE.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departaments> list = new List<Departaments>();
+            list.Add(new Departaments { id = 2, name = "Eletronics" });
+            list.Add(new Departaments { id = 2, name = "Fashion" });
+
+            return View(list.ToString());
+        }
+    }
+}
---
+++ SalesWeb.ASPNETCORE/Models/Entities/Departaments.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWeb.ASPNETCORE.Models.Entities
+{
+    public class Departaments
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
